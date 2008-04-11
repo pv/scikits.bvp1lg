a587d0db4cd5e902488adfe6467a6c8de4eb82f7
Pauli Virtanen <pav@iki.fi>
2008-04-11 22:47:33 +0300

    Try to play nicer along with numpy distutils...

---
+++ bvp/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import warnings
+from numpy.distutils.misc_util import Configuration
+from numpy.distutils.system_info import (get_info, AtlasNotFoundError,
+                                         BlasNotFoundError)
+
+def configuration(parent_package='', top_path=None):
+    blas_info = get_info('atlas')
+    if not blas_info:
+        warnings.warn(AtlasNotFoundError.__doc__)
+        blas_info = get_info('blas')
+        if not blas_info:
+            # Blas is required
+            print "\nError:\n%s\n" % BlasNotFoundError.__doc__
+            raise SystemExit(1)
+    
+    config = Configuration('bvp', parent_package, top_path)
+    config.add_extension('_colnew',
+                         sources=['../lib/colnew.pyf',
+                                  '../lib/colnew.f',
+                                  '../lib/dgesl.f',
+                                  '../lib/dgefa.f'],
+                         libraries=blas_info['libraries'],
+                         library_dirs=blas_info['library_dirs'])
+    config.add_extension('_mus',
+                         sources=['../lib/mus.pyf',
+                                  '../lib/mus1.f',
+                                  '../lib/mus2.f',
+                                  '../lib/mus3.f',
+                                  '../lib/dgesl.f',
+                                  '../lib/dgefa.f'],
+                         libraries=blas_info['libraries'],
+                         library_dirs=blas_info['library_dirs'])
+    config.add_data_dir('tests')
+    return config
--- setup.py
+++ setup.py
@@ -6,29 +6,21 @@
 try: import setuptools
 except ImportError: pass
 
-VERSION = "0.2.1.dev"
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+    config.add_subpackage('bvp')
+    config.add_data_files(('bvp', '*.txt'),
+                          ('bvp', 'TODO'))
+    return config
 
-import warnings
-from numpy.distutils.misc_util import Configuration
-from numpy.distutils.system_info import (get_info, AtlasNotFoundError,
-                                         BlasNotFoundError)
-
-def configuration(parent_package='', top_path=None):
-    blas_info = get_info('atlas')
-    if not blas_info:
-        warnings.warn(AtlasNotFoundError.__doc__)
-        blas_info = get_info('blas')
-        if not blas_info:
-            # Blas is required
-            print "\nError:\n%s\n" % BlasNotFoundError.__doc__
-            raise SystemExit(1)
-    
-    config = Configuration(
-        'bvp', parent_package, top_path,
-        package_path='bvp',
-        version=VERSION,
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(
+        name='bvp',
         author="Pauli Virtanen <pav@iki.fi>",
         author_email="pav@iki.fi",
+        version="0.2.1.dev",
         url="http://www.iki.fi/pav/software/bvp",
         license="Noncommercial, see LICENSE.txt",
         description="Boundary value problem solvers for ODEs",
@@ -38,31 +30,5 @@ Solvers for boundary value problems for ODEs.
 These are implemented by wrapping the COLNEW and MUS Fortran
 codes from netlib.org.""",
         install_requires=["numpy >= 1.0", "scipy >= 0.5"],
-        )
-
-    config.add_extension('_colnew',
-                         sources=['lib/colnew.pyf',
-                                  'lib/colnew.f',
-                                  'lib/dgesl.f',
-                                  'lib/dgefa.f'],
-                         libraries=blas_info['libraries'],
-                         library_dirs=blas_info['library_dirs'])
-
-    config.add_extension('_mus',
-                         sources=['lib/mus.pyf',
-                                  'lib/mus1.f',
-                                  'lib/mus2.f',
-                                  'lib/mus3.f',
-                                  'lib/dgesl.f',
-                                  'lib/dgefa.f'],
-                         libraries=blas_info['libraries'],
-                         library_dirs=blas_info['library_dirs'])
-
-    config.add_data_files('LICENSE.txt', 'TODO')
-    config.add_subpackage('bvp/tests')
-    
-    return config
-
-if __name__ == "__main__":
-    from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+        configuration=configuration,
+        )        

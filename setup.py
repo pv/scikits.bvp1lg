e0ef7a909ed7ec1d46234fd3f00de68937f89215
Pauli Virtanen <pav@iki.fi>
2018-03-10 20:33:43 +0100

    Set version to 0.2.10.dev0

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ MAINTAINER_EMAIL    = 'pav@iki.fi',
 URL                 = 'https://pv.github.io/scikits.bvp1lg'
 LICENSE             = 'Noncommercial, see LICENSE.txt'
 DOWNLOAD_URL        = 'https://pypi.python.org/pypi/scikits.bvp1lg'
-VERSION             = '0.2.9'
+VERSION             = '0.2.10.dev0'
 
 
 DOWNLOADABLE_FILES = [
